19ad43f3e528867a505c0cc4cb61d0781587bcec
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2023-03-10 17:02:36 +0600

    Added Doubly LL

--- Java/Main.java
+++ Java/Main.java
@@ -1,18 +1,14 @@
 public class Main{
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-        // ll.removeAtBeginning();
-        ll.insertAtBeginning(3);
-        ll.insertAtEnd(0);
-        ll.insertAtEnd(1);
+        DoublyLinkedList ll = new DoublyLinkedList();
+
+        ll.insertAtBeginning(0);
+        ll.insertAtBeginning(1);
         ll.insertAtEnd(2);
-        ll.insertAtMiddle(45, 2);
-
-        ll.print();
-        ll.removeWithValue(45);
-        // ll.removeAtMiddle(3);   
-        ll.print();
-
-        System.out.println(ll.find(45));
+        ll.insertAtMiddle(34, 2);
+        ll.printForward();
+        ll.removeAtMiddle(2);
+        ll.printForward();
+        System.out.println(ll.find(2));
     }
 }
